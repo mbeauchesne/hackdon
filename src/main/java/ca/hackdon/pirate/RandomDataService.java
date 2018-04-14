20b87a7e6df911c1681354d32a5b174dd3440a9b
beam1922 <maxime.beauchesne-jolin@usherbrooke.ca>
2018-04-14 18:31:04 -0400

    Begin fake

---
+++ src/main/java/ca/hackdon/pirate/RandomDataService.java
@@ -0,0 +1,14 @@
+package ca.hackdon.pirate;
+
+public class RandomDataService {
+
+
+
+	public String getFirstName() {
+//		String firstName = faker.name().firstName(); // Emory
+//		String lastName = faker.name().lastName(); // Barton
+//
+//		String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+		return "tt";
+	}
+}
